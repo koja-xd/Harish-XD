8063e066292298bc4b4b8ea48d5683e5b526ef0a
koja-xd <109822005+koja-xd@users.noreply.github.com>
2022-08-25 00:29:02 +0500

    Add files via upload

---
+++ harish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+pkg install figlet
+clear
+figlet install | lolcat
+sleep 2
+clear
+pkg install nano
+clear
+pkg install python
+clear
+pkg install python2
+clear
+pkg install libcurl
+clear
+gem install lolcat
+clear
+python harish.py
